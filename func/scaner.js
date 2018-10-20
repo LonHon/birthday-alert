f94ec7115f0a68ce3ebadb0550616d8132f3c06b
lonhon <longhong@ouchteam.com>
2018-10-20 22:22:41 +0800

    Update: scaner support solar

--- func/scaner.js
+++ func/scaner.js
@@ -3,8 +3,9 @@ const path = require('path') //系统路径模块
 const fs = require('fs') //文件模块
 const request = require('request')
 
-let todayLunar = null
+let TODAY_LUNAR = null
 
+// 获取今日农历信息
 function getTodayLunar () {
   const opt = {
     url: 'https://www.sojson.com/open/api/lunar/json.shtml',
@@ -15,7 +16,7 @@ function getTodayLunar () {
   return new Promise((resolve, reject) => {
     request.get(opt, (err, res, body) => {
       if (!err && res.statusCode == 200) {
-        todayLunar = JSON.parse(body)
+        TODAY_LUNAR = JSON.parse(body)
         resolve()
       } else {
         console.log(err)
@@ -24,8 +25,8 @@ function getTodayLunar () {
   })
 }
 
+// 读取json文件
 function getData() {
-  //读取json文件
   return new Promise((resolve, reject) => {
     const file = path.join(__dirname, '../data/data.json')
     fs.readFile(file, 'utf-8', function(err, data) {
@@ -38,35 +39,51 @@ function getData() {
   })
 }
 
-// 判断农历生日
-
-// 判断公历生日
-
-// 判断生日
-function birthIsNear(birth, type = 1) {
-  return new Promise((resolve, reject) => {
-    let next = 999
-    const birthArr = birth.split('/')
-    const m = birthArr[1]
-    const d = birthArr[2]
-    const today = type == 1 ? [todayLunar.data.lunarMonth, todayLunar.data.lunarDay] : [(new Date()).getMonth() +1, (new Date()).getDate()]
-    const lm = today[0]
-    const ld =  today[1]
-    if (m == lm) {
-      next = d - ld
+/* 判断两个日期差 
+*  @da = [12, 31],
+*  @db = [1, 3]
+*  ruturn 4
+*/
+function dateDiff(today, birth) {
+  let diff = 999
+  if (birth[0] === today[1]) {
+    diff = birth[1] - today[2]    
+  } else if ((birth[0] - today[1] === 1) || (birth[0] === 1 && today[1] === 12)) {
+    // 隔月 or 隔年
+    const now = new Date(today[0], today[1]-1, today[2])
+    let birthDay = new Date(today[0], birth[0]-1, birth[1])
+    if (today[1] === 12) { // 隔年+1
+      birthDay = new Date(today[0] + 1, birth[0]-1, birth[1])
     }
-    resolve(next)
+    diff = parseInt((birthDay - now) / 1000 / 60 /60 / 24)
+  }
+  return diff
+}
+
+// 返回生日
+function nextBirthday(birth, type = 0) {
+  return new Promise((resolve, reject) => {
+    const birthArr = birth.split('/')
+    const m = parseInt(birthArr[1])
+    const d = parseInt(birthArr[2])
+    // 获取今日公历or农历
+    const today = type === 0 ?
+      [TODAY_LUNAR.data.lunarMonth, TODAY_LUNAR.data.lunarDay]
+      :
+      [(new Date()).getFullYear(), (new Date()).getMonth() +1, (new Date()).getDate()]
+    resolve(dateDiff(today, [m, d]))
   })
 }
 
+// 扫描器
 async function scaner() {
   await getTodayLunar()
   const list = JSON.parse(await getData())
   let result = []
   for (const item of list) {
-    const n = await birthIsNear(item.birth)
+    const n = await nextBirthday(item.birth, item.type)
     if (n > -1 && n < 8) {
-      // 可用符号 '@ ， 。 . 卐 _'
+      // '@ ， 。 . 卐 _'
       result.push({
         name: item.name,
         day: n,
@@ -77,4 +94,5 @@ async function scaner() {
   result = result.sort((a, b) => a.day - b.day)
   return result
 }
+
 module.exports = scaner
